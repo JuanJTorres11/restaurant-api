c295600affbb651dbec4c6b9cd5fd18104eefa3e
Juan José Torres <juanjtorres@pm.me>
2021-07-24 16:53:54 -0500

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/JuanJTorres11/restaurant-api
+
+go 1.16
+
+require (
+	github.com/dgraph-io/dgo v1.0.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/genproto v0.0.0-20210722135532-667f2b7c528f // indirect
+	google.golang.org/grpc v1.39.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	// "context"
+	"log"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	// dgraphClient := dgo.NewDgraphClient(api.NewDgraphClient(conn))
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	http.ListenAndServe(":8000", r)
+}
